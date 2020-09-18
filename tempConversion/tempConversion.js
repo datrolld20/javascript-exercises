21c90f05e5aaa71b059f4417d367ca2ed0d06d3f
datrolld20 <datrolld20@gmail.com>
2020-09-18 11:44:52 -0400

    Completed tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return roundTwoDecimals(
+    (temp - 32) * 5 / 9
+  );
 }
 
-const ctof = function() {
+const ctof = function(temp) {
+  return roundTwoDecimals(
+    (temp * 9 / 5) + 32
+  );
+}
 
+const roundTwoDecimals = function(num) {
+  return parseFloat(num.toFixed(1));
 }
 
 module.exports = {
