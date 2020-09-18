cc0ce4f8b88802d2395d5874ff7a2d56b5439475
datrolld20 <datrolld20@gmail.com>
2020-09-18 11:05:33 -0400

    Completed removeFromArray exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+// My function will use a rest argument as the last arg (...arg).
+// A for loop will iterate through each argument and do the following:
+// While indexOf is not -1 (not present), array.splice(index, 1) to delete.
 
+const removeFromArray = function(array, ...args) {
+    let result = array;
+
+    console.log('Original array: ' + result);
+
+    for(let i = 0; i < args.length; i++) {
+        
+        while(result.indexOf(args[i]) !== -1) {
+            console.log('Current arg: ' + args[i]);
+            console.log('Index: ' + result.indexOf(args[i]));
+            result.splice(result.indexOf(args[i]), 1);
+        }
+    }
+
+    console.log(result);
+
+    return result;
 }
 
 module.exports = removeFromArray
